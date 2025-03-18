b27e1da66a86e8b9c34d2c2d958082777b880363
Tacokunchan <takutaku041201@icloud,com>
2025-03-18 12:34:39 +0900

    aaa

---
+++ app/lib/next_auth/options.ts
@@ -0,0 +1,27 @@
+import { debug } from "console";
+import { getProviders } from "next-auth/react";
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@next-auth/prisma-adapter"; 
+export const nextAuthOptions: NextAuthOptions = {
+    debug:false,
+    providers: [
+        GithubProvider({
+        clientId: process.env.GITHUB_ID!,
+        clientSecret: process.env.GITHUB_SECRET!,
+        }),
+    ],
+    adapter: PrismaAdapter(prisma),
+    callbacks:{
+        session: async ({session, user}) => {
+            return{
+                ...session,
+                user:{
+                    ...session.user,
+                    id: user.id,
+                },
+            };
+ 
+        },
+    }
+};
